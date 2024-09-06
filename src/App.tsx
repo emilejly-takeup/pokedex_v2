de25bd9690af9d6a9d7e79dca7e1edfc221db1f6
emilejly-takeup <emile.joly@take-up.fr>
2024-09-06 10:58:48 +0200

    ajout de la feature shiny + amélioration des performances + factorisation du code

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import PokemonData from "./interfaces/PokemonData";
 
 export default function App() {
   const [data, setData] = useState<PokemonData[] | []>([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [shinyState, setShinyState] = useState<boolean[]>([]);
 
   useEffect(() => {
     fetchData()
       .then((data: PokemonData[]) => {
         setData(data);
+        setShinyState(new Array(data.length).fill(false));
       })
       .catch((error: Error) => {
         console.error("There was an error!", error);
@@ -26,35 +28,73 @@ export default function App() {
     data && setCurrentIndex((currentIndex) => (currentIndex === data.length - 1 ? 0 : currentIndex + 1));
   };
 
-  function resolvePokemonIndex(wishedIndex: number) {
-    switch (wishedIndex) {
-      case -2:
-        return data && data[currentIndex === 0 ? data.length - 2 : currentIndex === 1 ? data.length - 1 : currentIndex - 2];
-      case -1:
-        return data && data[currentIndex === 0 ? data.length - 1 : currentIndex + wishedIndex];
-      case 1:
-        return data && data[currentIndex === data.length - 1 ? 0 : currentIndex + wishedIndex];
-      case 2:
-        return data && data[currentIndex === data.length - 2 ? 0 : currentIndex === data.length - 1 ? 1 : currentIndex + 2];
-      default:
-        return data && data[currentIndex];
-    }
+  const toggleShiny = (index: number) => {
+    setShinyState((prevShinyState) => prevShinyState.map((isShiny, i) => (i === index ? !isShiny : isShiny)));
+  };
+
+  function resolveIndex(wishedIndex: number) {
+    return (currentIndex + wishedIndex + data.length) % data.length;
   }
+
+  // Configuration des cartes (cette lise DOIT être paire)
+  const CARDSCONFIG = [
+    { indexOffset: -3, isHidden: true, isShinyOverride: true },
+    { indexOffset: -3, isHidden: true, isShinyOverride: false },
+    { indexOffset: -2, isHidden: false },
+    { indexOffset: -1, isHidden: false },
+    { indexOffset: 1, isHidden: false },
+    { indexOffset: 2, isHidden: false },
+    { indexOffset: 3, isHidden: true, isShinyOverride: false },
+    { indexOffset: 3, isHidden: true, isShinyOverride: true },
+  ];
 
   return (
     <div className="text-center h-screen content-center mx-auto max-w-fit">
       {data && data.length > 0 ? (
-        <>
-          <div className="flex items-center gap-8">
-            <PokemonCard isCurrentIndex={false} pokemon={resolvePokemonIndex(-2)} />
-            <PokemonCard isCurrentIndex={false} pokemon={resolvePokemonIndex(-1)} />
-            <NavigationButton text="←" onClick={handlePrev} />
-            <PokemonCard isCurrentIndex={true} pokemon={data[currentIndex]} />
-            <NavigationButton text="→" onClick={handleNext} />
-            <PokemonCard isCurrentIndex={false} pokemon={resolvePokemonIndex(1)} />
-            <PokemonCard isCurrentIndex={false} pokemon={resolvePokemonIndex(2)} />
-          </div>
-        </>
+        <div className="flex items-center gap-8">
+          {/* Rendu des cartes de gauche */}
+          {CARDSCONFIG.slice(0, CARDSCONFIG.length / 2).map(({ indexOffset, isHidden, isShinyOverride }, id) => {
+            const resolvedIndex = resolveIndex(indexOffset);
+            const isShiny = isShinyOverride !== undefined ? isShinyOverride : shinyState[resolvedIndex];
+            return (
+              <PokemonCard
+                key={id}
+                isCurrentIndex={false}
+                pokemon={data[resolvedIndex]}
+                isShiny={isShiny}
+                onToggleShiny={() => toggleShiny(resolvedIndex)}
+                isHidden={isHidden}
+              />
+            );
+          })}
+
+          {/* Rendu de la carte principale + nav */}
+          <NavigationButton text="←" onClick={handlePrev} />
+          <PokemonCard
+            isCurrentIndex={true}
+            pokemon={data[currentIndex]}
+            isShiny={shinyState[currentIndex]}
+            onToggleShiny={() => toggleShiny(currentIndex)}
+            isHidden={false}
+          />
+          <NavigationButton text="→" onClick={handleNext} />
+
+          {/* Rendu des cartes de droite */}
+          {CARDSCONFIG.slice(CARDSCONFIG.length / 2).map(({ indexOffset, isHidden, isShinyOverride }, id) => {
+            const resolvedIndex = resolveIndex(indexOffset);
+            const isShiny = isShinyOverride !== undefined ? isShinyOverride : shinyState[resolvedIndex];
+            return (
+              <PokemonCard
+                key={id + CARDSCONFIG.length / 2}
+                isCurrentIndex={false}
+                pokemon={data[resolvedIndex]}
+                isShiny={isShiny}
+                onToggleShiny={() => toggleShiny(resolvedIndex)}
+                isHidden={isHidden}
+              />
+            );
+          })}
+        </div>
       ) : (
         <p>Chargement...</p>
       )}
